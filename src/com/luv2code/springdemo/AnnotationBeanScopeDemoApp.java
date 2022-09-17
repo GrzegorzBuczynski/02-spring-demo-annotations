f4139db72a79ea7fe65f8c70ff7809579f38e4fa
GrzegorzBuczynski <grzegorzbuczynski93@gmail.com>
2022-09-17 22:33:32 +0200

    17.09.2022 beans lifecycle lesson

---
+++ src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,26 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+    public static void main(String[] args) {
+        
+        // load config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.Xml");
+
+        // retrive bean form container
+
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
+
+        Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+
+        boolean result = (theCoach == alphaCoach);
+
+        System.out.println("\nPointing the sane object: " + result);
+        System.out.println("\nMemory location for theCoach: " + theCoach);
+        System.out.println("\nMemory location for alphaCoach: " + alphaCoach + "\n");
+        
+        context.close();
+
+    }
+}
--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,24 +1,46 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+// @Scope("prototype")
 public class TennisCoach implements Coach {
 
     @Autowired
     @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
-    // @Autowired
-    // public TennisCoach(FortuneService theFortuneService) {
-    // fortuneService = theFortuneService;
-    // }
-
     // define a default constructor
     public TennisCoach() {
         System.out.println(">> TennisCoach: inside default constructor");
+    }
+
+    // define my init method
+    @PostConstruct
+    public void doMyStartuoStaff() {
+        System.out.println(">> TennisCoach: inside of doMyStartupStaff()");
+    }
+
+    // define my destroy method
+    @PreDestroy
+    public void doMyCleanuoStaff() {
+        System.out.println(">> TennisCoach: inside of doMyCleanuoStaff()");
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
     }
 
     // define setter method
@@ -31,14 +53,9 @@ public class TennisCoach implements Coach {
      */
     /* method name can be replaced by any given */
 
-    @Override
-    public String getDailyWorkout() {
-        return "Practice your backhand volley";
-    }
-
-    @Override
-    public String getDailyFortune() {
-        return fortuneService.getFortune();
-    }
+    // @Autowired
+    // public TennisCoach(FortuneService theFortuneService) {
+    // fortuneService = theFortuneService;
+    // }
 
 }
