3e4cc079cff22b2057edabc253ca86814f41ece9
GrzegorzBuczynski <grzegorzbuczynski93@gmail.com>
2022-09-20 21:32:53 +0200

    uodate 20.09.2022

--- src/com/luv2code/springdemo/AppSwimJavaConfigDemo.java
+++ src/com/luv2code/springdemo/AppSwimJavaConfigDemo.java
@@ -2,14 +2,14 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class AppSwimJavaDemo {
+public class AppSwimJavaConfigDemo {
     public static void main(String[] args) {
 
         // read spring config file
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConfigSport.class);
 
         // get bean from spring container
-        Coach theCoach = context.getBean("coachSwim", Coach.class);
+        CoachSwim theCoach = context.getBean("coachSwim", CoachSwim.class);
 
         // call a method on the bean
         System.out.println(theCoach.getDailyWorkout());
@@ -18,6 +18,11 @@ public class AppSwimJavaDemo {
 
         System.out.println(theCoach.getDailyFortune());
 
+        //call our new sim coach methods
+
+        System.out.println("email: " + theCoach.getEmail());
+        System.out.println("team: " + theCoach.getTeam());
+        
         // close the context
 
         context.close();
--- src/com/luv2code/springdemo/CoachSwim.java
+++ src/com/luv2code/springdemo/CoachSwim.java
@@ -1,11 +1,18 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CoachSwim implements Coach {
 
     private FortuneService fortuneService;
+
+    @Value("${foo.email}")
+    private String email;
+
+    @Value("${foo.team}")
+    private String team;
 
     public CoachSwim(FortuneService thefortuneService) {
         fortuneService = thefortuneService;
@@ -19,6 +26,14 @@ public class CoachSwim implements Coach {
     @Override
     public String getDailyFortune() {
         return fortuneService.getFortune();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTeam() {
+        return team;
     }
 
 }
